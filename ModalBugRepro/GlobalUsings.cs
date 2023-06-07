969ff90ccdd289f5cdc2243f36d6fdb4b86bd15c
Hans Bakker <hansmbakker+github@gmail.com>
2023-06-07 10:45:39 +0200

    initial commit

---
+++ ModalBugRepro/GlobalUsings.cs
@@ -0,0 +1,13 @@
+﻿global using ModalBugRepro.Models;
+global using ModalBugRepro.Services;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using ModalBugRepro.ViewModels;
+global using ModalBugRepro.Views;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Globalization;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
+global using System.Threading.Tasks;
